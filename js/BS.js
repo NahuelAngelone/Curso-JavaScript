61ee04220787bf9eca098d3c452e80dbe6baf06d
nahuelangelone <nahuelangelone94@gmail.com>
2023-12-17 23:04:39 -0300

    algo neox iniciado

--- js/BS.js
+++ js/BS.js
@@ -15,9 +15,10 @@ function Algoritmo(nombre, dificultad, recompensaBlock, hashred, nblocksxh, min,
 // Creo objetos usando el constructor
 const algo1 = new Algoritmo('XNA', 72759, 16176, 5510000, 1464, 0.004, 0.1);
 const algo2 = new Algoritmo('RVN', 69776, 2500, 5290000, 1440, 0.01, 0.4);
+const algo3 = new Algoritmo('NEOX',70000, 2500, 6290000, 1470, 0.01, 0.4)
 
 // Almaceno los objetos en un array llamado algoritmos
-const algoritmos = [algo1, algo2];
+const algoritmos = [algo1, algo2, algo3];
 
 // Array de nombres de algoritmos
 const nombresAlgoritmos = ['RVN', 'XNA', 'NEOX', 'CLORE', 'NEURAI', 'SATOX'];
